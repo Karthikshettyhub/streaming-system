13c9f6f461db07cd2805e61a5854d41e4eb69f53
karthikshettyhubb <karthikshetty8546@gmail.com>
2025-09-21 14:02:25 +0530

    routes and controller begins

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,10 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+
+const registerUser = asyncHandler( async (req,res) => {
+    res.status(200).json({
+        Message: "working function"
+    })
+})
+
+export {registerUser}
